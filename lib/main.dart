1d349203ceb0770f4bf500f708dd099823b28de9
ricarthlima <rrsl@cin.ufpe.br>
2024-09-27 00:01:28 -0300

    v1.5 - imagens e estilizações

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,64 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                "SISTEMA DE GESTÃO DE CONTAS",
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 32,
-                ),
-                textAlign: TextAlign.center,
+        body: Stack(
+          children: [
+            Image.asset("assets/banner.png"),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Image.asset("assets/stars.png"),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 128),
+                  Image.asset(
+                    "assets/logo.png",
+                    width: 128,
+                  ),
+                  const SizedBox(height: 32),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Text(
+                        "Sistema de gestão de contas",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 32,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text("E-mail")),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(label: Text("Senha")),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 32),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                            Color(0xFFFFa902),
+                          ),
+                        ),
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              TextFormField(
-                decoration: const InputDecoration(label: Text("E-mail")),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(label: Text("Senha")),
-                obscureText: true,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Entrar"),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
